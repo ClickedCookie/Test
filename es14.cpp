75dca969d08934e3ef18cc684355cd5ea1e0c6d5
frarame <155736289+frarame@users.noreply.github.com>
2025-02-27 00:43:28 +0100

    Last add

--- es14.cpp
+++ es14.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <vector>
+
+
+
+int count_mimax(std::vector<int> vec, int max, int min);
 
 int main(){
 
-
-    std::cout<<"Hello World!"<<std::endl;
-
-
+    int n;
+    std::cin >> n;
+    std::vector<int> vec(n);
+    int min, max;
+    std::cin >> min;
+    std::cin >> max;
+    for (int i = 0; i < vec.size(); i++){
+        std::cin >> vec[i];
+    }
+    int res = count_mimax(vec, max, min);
+    std::cout << res << std::endl;
 
     return 0;
+}
+
+int count_mimax(std::vector<int> vec, int max, int min){
+    int count = 0;
+    for(int i = 0; i < vec.size(); i++){
+        if(vec[i]>min and vec[i]<max){
+            count++;
+        }
+    }
+    return count;
 }
